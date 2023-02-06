e936e7d71c39d86feea20eaa06d85b91208f063c
jsh <asin@naver.com>
2023-02-06 19:57:13 +0900

    1

---
+++ mybbs/src/main/java/org/greenp/mybbs/MybbsApplication.java
@@ -0,0 +1,13 @@
+package org.greenp.mybbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybbsApplication.class, args);
+	}
+
+}
---
+++ mybbs/src/test/java/org/greenp/mybbs/MybbsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.greenp.mybbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
